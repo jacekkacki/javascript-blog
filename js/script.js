77f7cc4ab5369d7ba588f4fd73e2b70d5f339ce7
Jacek Kącki <jkacki@gmail.com>
2019-08-30 21:25:27 +0200

    Clear code script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,6 @@
 {
   ("use strict");
-  /*
-document.getElementById('test-button').addEventListener('click', function(){
-    const links = document.querySelectorAll('.titles a');
-    console.log('links:', links);
-  }); */
+
   const titleClickHandler = function(event) {
     event.preventDefault();
     console.log("Link was clicked!");
